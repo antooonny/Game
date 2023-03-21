d409d29ce3f4ee81a177b23b4fdb58090b2a59c2
antooonny <95007105+antooonny@users.noreply.github.com>
2023-03-20 22:57:20 -0700

    enemy spawn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from sys import exit
+import random
 
 def display_score():
     curr_time = int(pygame.time.get_ticks()/1000) - start_time
@@ -7,6 +8,19 @@ def display_score():
     score_rec = score_surface.get_rect(center=(400, 50))
     screen.blit(score_surface, score_rec)
     return curr_time
+
+def obstacle_movement(o_list):
+    if o_list:
+        for obstacle_rect in o_list:
+            obstacle_rect.x -= 5
+            screen.blit(snail_surface, obstacle_rect)
+
+        o_list = [obstacle for obstacle in o_list if obstacle.x > 0]
+        return o_list
+
+    else:
+        return []
+
 pygame.init()
 screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption('Experimental Game!')
@@ -27,8 +41,10 @@ start_rec = start_surf.get_rect(center = (400, 350))
 sky = pygame.image.load('graphics/Sky.png').convert()
 ground = pygame.image.load('graphics/ground.png').convert()
 
+
 snail_surface = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
 snail_rectangle = snail_surface.get_rect(midbottom = (40, 300))
+obstacle_list = []
 
 player = pygame.image.load('graphics/Player/player_walk_1.png').convert_alpha()
 player_rectangle = player.get_rect(midbottom = (300, 300))
@@ -40,17 +56,27 @@ player_dupe_rect = player_dupe.get_rect(center = (400, 200))
 snail_x = 0
 score2 = 0
 
+obstacle_timer = pygame.USEREVENT + 1
+pygame.time.set_timer(obstacle_timer, 1400)
+
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+        if event.type == obstacle_timer and game_active:
+            print('test')
+            obstacle_list.append(snail_surface.get_rect(bottomright = (random.randint(1100,1500), 300)))
+
     if game_active:
+
+
+
         screen.blit(sky, (0,0))
         screen.blit(ground, (0,300))
         screen.blit(snail_surface, snail_rectangle)
         score2 = display_score()
-        snail_rectangle.left+=2
 
         mouse_pos = pygame.mouse.get_pos()
         if player_rectangle.collidepoint(mouse_pos):
@@ -62,6 +88,9 @@ while True:
 
         player_gravity+=1
         player_rectangle.y += player_gravity
+
+        obstacle_list = obstacle_movement(obstacle_list)
+
         if player_rectangle.bottom >= 300: player_rectangle.bottom = 300
         screen.blit(player, player_rectangle)
 
@@ -73,9 +102,9 @@ while True:
         screen.fill((94,129,162))
         screen.blit(player_dupe, player_dupe_rect)
         screen.blit(start_surf, start_rec)
-        score_mes = test_font.render(f'Your score: {score}', False, 'black')
-        score_mes_rec = score_mes.get_rect(center = (300, 300))
-        if score == 0:
+        score_mes = test_font.render(f'Your score: {score2}', False, 'black')
+        score_mes_rec = score_mes.get_rect(center = (400, 320))
+        if score2 == 0:
             screen.blit(title_surf, title_rec)
 
         else:
